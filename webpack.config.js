356d8bc4d147b891e364a7e6e8853e50ab960dc8
Roman Novikov <r.novikov.zp.ua@gmail.com>
2023-08-09 22:02:01 +0300

    Create basic structure of project and confige webpack

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'), 
+    filename: 'main.js',
+  },
+    plugins: [
+        new CleanWebpackPlugin(),
+  ],
+  module: {
+    mode: 'development',
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, 'dist'), 
+    port: 8080,
+  },
+};
