f28327c476722b337215b20b31697a110f4332fb
Roman Novikov <r.novikov.zp.ua@gmail.com>
2023-08-17 11:31:00 +0300

    Separate UI logic and creating elements into different modules

--- src/UI.js
+++ src/UI.js
@@ -1,7 +1,10 @@
-import elements from './elements.js'
+import element from './elements.js'
 //methods:
-//  addForm()
-//  editForm(target)
+//  addForm,
+//  editForm(object),
+//  taskCard(object),
+//  projectLabel(object),
+//  listItem(object)
 
 const display = (() => {
 
@@ -76,10 +79,12 @@ const display = (() => {
 
     const expand = (target) => {
         target.classList.add('expand');
+        target.classList.remove('no-visible');
     }
 
     const collapse = (target) => {
         target.classList.remove('expand');
+        target.classList.add('no-visible');
     }
 
     const dropdown = (item) => {
@@ -92,161 +97,6 @@ const display = (() => {
             show(item.counter);
             item.button.value = 'unchecked';
         };
-    };
-
-    const indicatorValue = (priority, target) => {
-        switch (priority) {
-            case 'high':
-                target.classList.add('red');
-                break;
-            case 'normal':
-                target.classList.add('yellow');
-                break;
-            case 'low':
-                target.classList.add('green');
-                break;
-        }
-    }
-
-    const make = (object) => {
-
-        const title = object.name;
-        const description = object.description;
-        const dueDate = object.dueDate;
-        const priority = object.priority;
-        const isComplete = object.isComplete;
-        const type = object.type;
-        const techName = object.techName;
-
-        const card = ()=> {
-            const cardBody = document.createElement('div');
-            cardBody.classList.add('task-card');
-            cardBody.id = techName + '-card';
-
-            const cardTitle = document.createElement('h3');
-            cardTitle.textContent = title;
-            cardBody.appendChild(cardTitle);
-
-            const editButton = document.createElement('button');
-            editButton.title = 'Edit task';
-            editButton.classList.add('control', 'edit');
-            cardBody.appendChild(editButton);
-
-            const closeButton = document.createElement('button');
-            closeButton.title = 'Close';
-            closeButton.classList.add('control delete');
-            cardBody.appendChild(closeButton);
-
-            const descPara = document.createElement('p');
-            descPara.textContent = description;
-            cardBody.appendChild(descPara);
-
-            const indicator = document.createElement('div');
-            indicator.classList.add('priority-indicator');
-            indicatorValue(priority, indicator);
-            cardBody.appendChild(indicator);
-
-            const dueDateTitle = document.createElement('h4');
-            dueDateTitle.textContent = 'Due Date:';
-            cardBody.appendChild(dueDateTitle);
-
-            const dueDateValue = document.createElement('div');
-            dueDateValue.classList.add('date');
-            dueDateValue.textContent = dueDate;
-            cardBody.appendChild(dueDateValue);
-
-            const completeTitle = document.createElement('h4');
-            completeTitle.textContent = 'Complete';
-            cardBody.appendChild(completeTitle);
-
-            const completeCheckbox = document.createElement('button');
-            completeCheckbox.classList.add('control', 'checkbox');
-            completeCheckbox.title = 'Mark complete'
-            if (isComplete) {
-                cardBody.classList.add('checked');
-            }
-            cardBody.appendChild(completeCheckbox);
-
-            return cardBody;
-        };
-
-        const label = () => {
-            const labelBody = document.createElement('button');
-            labelBody.classList.add('task-label');
-            labelBody.id = techName + '-button';
-
-            const nameButton = document.createElement('button');
-            nameButton.classList.add('label-name');
-            nameButton.textContent = title;
-            labelBody.appendChild(nameButton);
-
-            const editButton = document.createElement('button');
-            editButton.classList.add('control', 'edit');
-            labelBody.appendChild(editButton);
-
-            const deleteButton = document.createElement('button');
-            deleteButton.classList.add('control', 'delete');
-            labelBody.appendChild(deleteButton);
-
-            return labelBody;
-        };
-
-        const listItem = () => {
-            const listItemBody = document.createElement('div');
-            listItemBody.classList.add('list-element');
-            cardBody.id = techName + '-item';
-
-            const listIndicator = document.createElement('div');
-            listIndicator.classList.add('list-indicator');
-            indicatorValue(priority, listIndicator);
-            listItemBody.appendChild(listIndicator);
-
-            const listName = document.createElement('p');
-            listName.textContent = title;
-            listItemBody.appendChild(listName);
-
-            const listDesc = document.createElement('p');
-            listDesc.textContent = description;
-            listItemBody.appendChild(listDesc);
-
-            const listDueDate = document.createElement('p');
-            listDueDate.textContent = dueDate;
-            listItemBody.appendChild(listDueDate);
-
-            const listDetails = create('button', 'control');
-            listDetails.classList.add('details');
-            listDetails.text.content = 'Details';
-            listDetails.title = 'Show details';
-            listDetails.id = techName;
-            listItemBody.appendChild(listDetails);
-
-            const listIsComplete = create('button', 'control');
-            listIsComplete.classList.add('checkbox');
-            listIsComplete.title = 'Mark complete'
-            if (isComplete) {
-                listItemBody.classList.add('checked');
-            }
-            listItemBody.appendChild(listIsComplete);
-
-            const listEdit = create('button', 'control');
-            listEdit.classList.add('edit');
-            listEdit.title = 'Edit task';
-            listItemBody.appendChild(listEdit);
-
-            const listDelete = create('button', 'control');
-            listDelete.classList.add('edit');
-            listDelete.title = 'Delete task';
-            listItemBody.appendChild(listDelete);
-
-            return listItemBody;
-        }
-
-        return {
-            card,
-            label,
-            listItem
-        }
-
     };
 
     const addProjectList = (label) => {
@@ -262,17 +112,21 @@ const display = (() => {
     }
 
     const populateList = (array, nodeList) => {
-        array.forEach(object => {
-            const item = make(object);
-            switch (nodeList.list.id) {
-                case 'project-list':
-                    nodeList.list.appendChild(item.label);
-                    addProjectList(item.label);
-                    break;
-                default:
-                    nodeList.list.appendChild(item.listItem);
-                    break;
-            }
+        const listName = nodeList.name
+        const list = nodeList.list;
+        const action = () => {};
+
+        switch (listName) {
+            case 'projects':
+                action = element.projectLabel;
+                break;
+            default:
+                action = element.listItem;
+        }
+
+        array.forEach((object) => {
+            item = action(object);
+            list.appendChild(item);
         })
     }
 
@@ -291,21 +145,20 @@ const display = (() => {
     const callCard = (e, array) => {
         const techName = e.target.id;
         const object = array.find(obj => obj.techName = techName);
-        const item = make(object);
-        const card = item.card;
+        const card = element.taskCard(object);
         showCardFrame(card);
         return(card);
     }
 
     const callAddForm = () => {
-        const form = elements.addForm();
+        const form = element.addForm();
         showCardFrame(form);
         return form;
     }
 
     const callEditForm = (target) => {
         const object = target;
-        const form = elements.editForm(object);
+        const form = element.editForm(object);
         showCardFrame(form);
         return form
     }
@@ -327,7 +180,6 @@ const display = (() => {
         callEditForm,
         hideCardFrame,
         populateList,
-        nodeListHolder
     };
     
 })();
